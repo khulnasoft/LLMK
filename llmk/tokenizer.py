f005188864238b6f7da63685e0b8c452805c4e96
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-02-26 23:01:38 +0000

    init commit

--- llmk/tokenizer.py
+++ llmk/tokenizer.py
@@ -1,7 +1,11 @@
-from sentencepiece import SentencePieceProcessor
+# Copyright (c) Khulnasoft Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llmk 2 Community License Agreement.
+
+import os
 from logging import getLogger
 from typing import List
-import os
+
+from sentencepiece import SentencePieceProcessor
 
 
 logger = getLogger()
--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
-from setuptools import setup, find_packages
+# Copyright (c) Khulnasoft Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llmk 2 Community License Agreement.
 
-setup(name="llmk", version="0.0.0", packages=find_packages())
+from setuptools import find_packages, setup
+
+
+def get_requirements(path: str):
+    return [l.strip() for l in open(path)]
+
+
+setup(
+    name="llmk",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
